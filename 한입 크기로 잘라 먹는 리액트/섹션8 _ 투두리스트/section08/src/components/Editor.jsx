55d694f13031f0bad403d4b83b0b9f2da73be2d6
haaazz <ez703@naver.com>
2024-06-25 03:47:53 +0900

    240625 한입크기로잘라먹는리액트 8.2-8.3

---
+++ 한입 크기로 잘라 먹는 리액트/섹션8 _ 투두리스트/section08/src/components/Editor.jsx
@@ -0,0 +1,13 @@
+import "./Editor.css/";
+
+const Editor = () => {
+  return (
+    <div className="Editor">
+      {" "}
+      <input placeholder="Todo 생성하기.." />
+      <button>생성</button>{" "}
+    </div>
+  );
+};
+
+export default Editor;
